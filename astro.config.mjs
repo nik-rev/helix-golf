25c1df42e57618fbb7a69a5b931f6270a751d83a
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2024-12-10 23:09:07 +0000

    feat: basic setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,44 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  markdown: {
+    rehypePlugins: [
+      rehypeHeadingIds,
+
+      [
+        rehypeExternalLinks,
+        {
+          content: {
+            type: "text",
+            value: " ↗",
+          },
+          properties: {
+            target: "_blank",
+          },
+          rel: ["noopener"],
+        },
+      ],
+      [rehypeAutolinkHeadings, { behavior: "wrap" }],
+    ],
+  },
+  integrations: [
+    starlight({
+      title: "Helix Golf",
+      social: {
+        github: "https://github.com/nikitarevenco/helix-golf",
+      },
+      sidebar: [
+        {
+          label: "Helix Golf",
+          autogenerate: { directory: "helix-golf" },
+        },
+      ],
+      customCss: ["./src/globals.css"],
+    }),
+  ],
 });
