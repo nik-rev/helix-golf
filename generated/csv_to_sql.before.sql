0d73d090bfa5f6a6a850117b277d361099191f18
nik-rev <nik-rev@users.noreply.github.com>
2025-05-27 00:01:00 +0000

    deploy: 04ab95669903f0d15946c401ace6296869d3bd28

--- generated/csv_to_sql.before.sql
+++ generated/csv_to_sql.before.sql
@@ -1,3 +1,6 @@
 INSERT INTO `database`.`table` (`id` ,`item` ,`cost` ,`location`) VALUES ('id 1','Item 1','cost 1','Location 1');
+ world
 INSERT INTO `database`.`table` (`id` ,`item` ,`cost` ,`location`) VALUES ('id 2','Item 2','cost 2','Location 2');
+ world
 INSERT INTO `database`.`table` (`id` ,`item` ,`cost` ,`location`) VALUES ('id 10','Item 10','cost 10','Location 10');
+ world
